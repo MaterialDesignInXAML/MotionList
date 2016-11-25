2ee694ef373ebc15c1f0bbc305a80f3922054dc6
ButchersBoy <james.willock@mulhollandsoftware.com>
2016-11-25 13:49:54 +0000

    illustrate how to watch ordering

--- MotionList/MainWindow.xaml.cs
+++ MotionList/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Dragablz;
 
 namespace SweetNewItemsControl
 {
@@ -20,9 +21,41 @@ namespace SweetNewItemsControl
     /// </summary>
     public partial class MainWindow : Window
     {
+        private object[] _order;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            AddHandler(DragablzItem.DragStarted, new DragablzDragStartedEventHandler(ItemDragStarted), true);
+            AddHandler(DragablzItem.DragCompleted, new DragablzDragCompletedEventHandler(ItemDragCompleted), true);
+        }
+
+        private void ItemDragStarted(object sender, DragablzDragStartedEventArgs e)
+        {
+            var frameworkElement = (FrameworkElement)e.DragablzItem.DataContext;
+
+            System.Diagnostics.Trace.WriteLine($"User started to drag item with Tag {frameworkElement.Tag}.");           
+        }
+
+        private void ItemDragCompleted(object sender, DragablzDragCompletedEventArgs e)
+        {
+            var frameworkElement = (FrameworkElement)e.DragablzItem.DataContext;
+            System.Diagnostics.Trace.WriteLine($"User finished dragging item with Tag {frameworkElement.Tag}.");
+
+            if (_order == null) return;
+
+            System.Diagnostics.Trace.Write("Order is now: ");
+            foreach (var fe in _order.OfType<FrameworkElement>())
+            {
+                System.Diagnostics.Trace.Write(fe.Tag + " ");
+            }
+            System.Diagnostics.Trace.WriteLine("");
+        }
+
+        private void StackPositionMonitor_OnOrderChanged(object sender, OrderChangedEventArgs e)
+        {
+            _order = e.NewOrder;
         }
     }
 }
